6293fb3ccc01cf2a17a37c8266e650394c8e581a
Muhammad Naveed Khan <naveed@Apples-MacBook-Pro.local>
2023-09-24 23:08:17 +0500

    Add tailwind successfully.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
 import './App.css';
-import Header from './components/UI/Header';
-import Sidebar from './components/UI/Sidebar';
-import Footer from './components/UI/Footer';
-import HelloMsg from './components/HelloMsg';
-
-function MyButton(props) {
-
-  console.log('props = ',  props);
-
-  function helloBro(){
-    alert('Hello Brother From MyButton')
-  }
-
-  return (
-    <button onClick={myEvent}>
-      I'm a button
-    </button>
-  );
-}
-
 
 function App() {
 
-  let myData = [
-    {id: 1, stdName: "Naveed", age: 36},
-    {id: 2, stdName: "Noman", age: 33},
-    {id: 3, stdName: "Ali", age: 30},
-    {id: 4, stdName: "Rehman", age: 32},
-    {id: 5, stdName: "Wasim", age: 41},
-  ];
-
-  function helloBro(){
-    alert('Hello Brother From App Component');
-  }
 
   return (
     <>
       <div className='App'>
 
-      <HelloMsg data = { myData } />
+        <h1 className='bg-green-400'> Hello Brother </h1>
 
-      <MyButton myEvent="helloBro" />
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
 
-        {/* <Header />
-        <Sidebar />
-        <Footer /> */}
       </div>
     </>
   );
